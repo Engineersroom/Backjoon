0c1831d4bbca416b0136714c96103e6b0fb27e22
Engineersroom <115200460+Engineersroom@users.noreply.github.com>
2022-11-09 18:47:43 +0900

    수정사항 ㅇㅋ

--- C/basic_math_1/BJ10250.c
+++ C/basic_math_1/BJ10250.c
@@ -5,39 +5,31 @@ int main()
     int h = 0;
     int w = 0;
     int caseNum = 0;
-    int Num =0;
+    int Num = 0;
 
     int H = 0;
     int W = 0;
-    scanf("%d",&caseNum);
+    scanf("%d", &caseNum);
 
-    for (int i = 0 ; i < caseNum ; i ++ )
+    for (int i = 0; i < caseNum; i++)
     {
-        scanf("%d %d %d",&h,&w,&Num);//h 층 w폭 num 몇번쨰손님?
+        scanf("%d %d %d", &h, &w, &Num); // h 층 w폭 num 몇번쨰손님?
 
-        H = Num%h;
-        if (Num == h)
+        H = Num % h;
+        if (Num % h == 0)
         {
             H = h;
         }
-        if (h == 1)
-        {
-            H = 1;
-        }
-        W = Num/h+1;
-        if (h==1)
-        {
-            W=Num;
-        }
+        W = ((double)Num / h) + 0.99;
+        // printf("H : %d ",H);
+        // printf("W : %d \n",W);
         if (W < 10)
         {
-            printf("%d0%d\n",H,W);
+            printf("%d0%d\n", H, W);
         }
         else
         {
-           printf("%d%d\n",H,W); 
+            printf("%d%d\n", H, W);
         }
-        
     }
-
 }
