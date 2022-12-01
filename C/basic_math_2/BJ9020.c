87138db1112b03bdcbb98b2741b77b5fa9c45dd8
Engineersroom <115200460+Engineersroom@users.noreply.github.com>
2022-12-01 10:46:57 +0900

    company

---
+++ C/basic_math_2/BJ9020.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//골드바흐의 추측
+//2보다 큰 짝수는 두 소수의 합으로 나타낼 수 있다.
+int main()
+{
+    int Ncase = 0;
+    int Ninput = 0;
+
+    scanf("%d",&Ncase);
+    for (int n = 0; n < Ncase ; n++)
+    {
+        scanf("%d",Ninput);
+        
+    }
+
+}
