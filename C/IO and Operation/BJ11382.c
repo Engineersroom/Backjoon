49175ded389f01f28e87928daedbf22dba80e481
Engineersroom <eunsang284@gmail.com>
2023-03-04 17:05:12 +0900

    1단계 새로운 문제 해결, 변수형 lld

--- C/IO and Operation/BJ11382.c
+++ C/IO and Operation/BJ11382.c
@@ -2,9 +2,13 @@
 
 int main()
 {
-    unsigned long long sum = 0;
+    unsigned long long int sum = 0;
+    unsigned long long int a = 0;
 
     for (int i = 0; i < 3; i++)
     {
+        scanf("%lld", &a);
+        sum = sum + a;
     }
+    printf("%lld",sum);
 }
--- C/IO and Operation/tempCodeRunnerFile.c
+++ C/IO and Operation/tempCodeRunnerFile.c
@@ -2,8 +2,13 @@
 
 int main()
 {
- printf("\\    /\\\n");
- printf(" )  ( ')\n");
- printf("(  /  )\n");
- printf(" \\(__)|");
+    unsigned long long sum = 0;
+    unsigned long long a = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%d", &a);
+        sum = sum + a;
+    }
+    printf("%d",sum);
 }
