847c3c99797fefbef161d9fb341427cc2950f19b
Engineersroom <eunsang284@gmail.com>
2023-04-17 01:20:32 +0900

    BJ10812 Clear

---
+++ C/Advanced/BJ10812.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int Basket[100];
+int tmpBasket[100];
+int main()
+{
+    int N;
+    int M;
+    int I, J, K;
+
+    scanf("%d", &N); // Max Basket;
+
+    scanf("%d", &M); // Max Case;
+
+    for (int i = 0; i < N; i++)
+    {
+        Basket[i] = i+1;
+        // tmpBasket[i] = i;
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%d %d %d", &I, &J, &K); // Begin, End , Middle
+        I = I - 1;
+        J = J - 1;
+        K = K - 1;
+        int a = 0;
+
+        for (int tmp = 0; tmp < (J - I + 1); tmp++)
+        {
+            if ((K + tmp) <= J)
+            {
+                tmpBasket[tmp] = Basket[K + tmp];
+                a = tmp;
+            }
+            else // if (K+tmp > J)
+            {
+                tmpBasket[tmp] = Basket[I + tmp - a - 1];
+            }
+        }
+        for (int tmp = 0; tmp < (J - I + 1); tmp++)
+        {
+            Basket[I+tmp] = tmpBasket[tmp];
+        }
+        //Debug
+        // for (int tmp = 0; tmp < N ; tmp++)
+        // {
+        //     printf("%d ",Basket[tmp]);
+
+        // }
+        // printf("\r\n");
+        //Debug
+    }
+    for (int tmp = 0 ; tmp < N ; tmp ++)
+    {
+        printf("%d ",Basket[tmp]);
+    }
+    
+}
